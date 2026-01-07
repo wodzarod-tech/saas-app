4d640d6efba87dfcc0c011c017c5e85a9333e08a
Zarbio <zarod2019@gmail.com>
2026-01-07 15:36:05 -0500

    Add select Subject in form

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,9 +1,16 @@
 //localhost:3000/companions/new
 // Companion Builder page
 
+import CompanionForm from "@/components/CompanionForm"
+
 const NewCompanion = () => {
   return (
-    <div>NewCompanion</div>
+    <main>
+      <article>
+        <h1>Companion Builder</h1>
+        <CompanionForm />
+      </article>
+    </main>
   )
 }
 
