f2844f205c2d25db35b7a146baec9dcb46084c64
Zarbio <zarod2019@gmail.com>
2026-01-07 17:52:42 -0500

    Add Clerk Authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -21,8 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
-        <Navbar />
-        {children}</body>
+        <ClerkProvider appearance={{ variables: { colorPrimary: "#fe5933" } }}>
+          <Navbar />
+          {children}
+        </ClerkProvider>
+      </body>
     </html>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,6 +6,7 @@ className="navbar" from globals.css
 import Link from "next/link"
 import Image from "next/image"
 import NavItems from "./NavItems"
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 
 const Navbar = () => {
   return (
@@ -22,7 +23,14 @@ const Navbar = () => {
         </Link>
         <div className="flex items-center gap-8">
             <NavItems />
-            <p>Sign In</p>
+            <SignedOut>
+                <SignInButton>
+                    <button className="btn-signin">Sign In</button>
+                </SignInButton>
+            </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
         </div>
     </nav>
   )
