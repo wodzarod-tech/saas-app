0db215580caa46f0fb92f9ec491ad85961919f86
Zarbio <zarod2019@gmail.com>
2026-01-07 20:46:05 -0500

    Add Clerk Billing

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,9 +1,13 @@
 //localhost:3000/subscription
 // Subscription page
 
+import { PricingTable } from "@clerk/nextjs"
+
 const Subscription = () => {
   return (
-    <div>Subscription</div>
+    <div>
+      <PricingTable />
+    </div>
   )
 }
 
