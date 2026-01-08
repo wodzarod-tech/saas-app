8277b60c11d5299999527d498d0764f273f7bc87
Zarbio <zarod2019@gmail.com>
2026-01-08 17:00:51 -0500

    Companion Library - SubjectFilter

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -16,6 +16,7 @@ const SearchInput = () => {
 
     // modify URL bar based on what we typing in the search input
     // what we typing is querying to the the database
+    // http://localhost:3000/companions?topic=math
     useEffect(() => {
         const delayDebounceFn = setTimeout(() => {
             if(searchQuery) {
@@ -42,8 +43,7 @@ const SearchInput = () => {
     }, [searchQuery, router, searchParams, pathname]);
 
     return (
-        <div className="relative border border-black rounded-lg items-center
-        flex gap-2 px-2 py-1 h-fit">
+        <div className="relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit">
             <Image src="icons/search.svg" alt="search" width={15} height={15} />
             <input
                 placeholder="Search companions... "
