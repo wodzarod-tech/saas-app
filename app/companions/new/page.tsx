dad86abc0142f7aa86de4163f4fb9dbed1571309
Zarbio <zarod2019@gmail.com>
2026-01-08 08:49:16 -0500

    Supabase implementation - Build Your Companion

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -2,11 +2,18 @@
 // Companion Builder page
 
 import CompanionForm from "@/components/CompanionForm"
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const NewCompanion = () => {
+const NewCompanion = async() => {
+
+  // when logout, then user dont must be to create a form
+  const { userId } = await auth();
+  if(!userId) redirect('/sign-in');
+
   return (
-    <main>
-      <article>
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+      <article className="w-full gap-4 flex flex-col">
         <h1>Companion Builder</h1>
         <CompanionForm />
       </article>
--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -24,6 +24,8 @@ import {
 } from "@/components/ui/select" // npx shadcn@latest add select
 import { subjects } from "@/constants"
 import { Textarea } from "./ui/textarea"
+import { createCompanion } from "@/lib/actions/companion.actions"
+import { redirect } from "next/navigation"
 
 const formSchema = z.object({
   name: z.string().min(1, { message: 'Companion is required.'}),
@@ -50,10 +52,20 @@ const CompanionForm = () => {
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async(values: z.infer<typeof formSchema>) => {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    //console.log(values)
+    const companion = await createCompanion(values);
+
+    if(companion) {
+      redirect(`/companions/${companion.id}`);
+    } else {
+      console.log('Failed to create a companion');
+      redirect('/');
+      // Invalid redirect arguments. Please use a single argument URL, e.g. res.redirect('/destination') or use a status code and URL, e.g. res.redirect(307, '/destination').
+
+    }
   }
 
   return (
